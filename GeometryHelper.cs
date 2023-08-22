a53ffa529d192395fbc53ae8132c777d8db0ba37
sananeo0 <sananeo@code.edu.az>
2023-08-22 15:06:56 +0400

    Add project files.

---
+++ GeometryHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tap1
+{
+    public static class GeometryHelper
+    {
+        public static decimal Triangle(decimal aSide, decimal bSide, decimal cSide)
+        {
+            decimal semi_perimeter = (aSide + bSide + cSide) / 2;
+            return (decimal)Math.Sqrt((double)(semi_perimeter * (semi_perimeter - aSide) * (semi_perimeter - bSide) * (semi_perimeter - cSide)));
+        }
+
+        public static decimal CirclePerimeter(decimal radius)
+        {
+            return (decimal)Math.PI * radius*2;
+        }
+
+        public static decimal QuadrilateralArea(decimal leftSide, decimal rightSide, decimal downSide, decimal upSide)
+        {
+            return leftSide * upSide;
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using tap1;
+
+namespace HomeTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal triangle = GeometryHelper.Triangle(3, 4, 5);
+            Console.WriteLine(triangle);
+            decimal circlePerimeter = GeometryHelper.CirclePerimeter(5.0m);
+            Console.WriteLine(circlePerimeter);
+            decimal Quadrilateral = GeometryHelper.QuadrilateralArea(2, 2, 3, 3);
+            Console.WriteLine(Quadrilateral);
+
+        }
+    }
+}
